06df43f67baf108eef007e382d46b95f101efaeb
iamrubyist <78143516+TakagiMasahiro1@users.noreply.github.com>
2021-03-17 18:19:02 +0900

    authenticate_user!
    what
    authenticate_user!の記述
    why
    ログイニューザーのみ処理をしたい

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_picture, only: [:edit, :show]
   before_action :move_to_index, except: [:index, :show, :search]
 
